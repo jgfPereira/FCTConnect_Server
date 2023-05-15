8cc3145ca976c387eef8aa9207fc176f59cf4261
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-05-15 16:08:44 +0100

    Completed new password operation

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/NewPasswordResource.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/NewPasswordResource.java
@@ -2,9 +2,8 @@ package pt.unl.fct.di.apdc.chatfct.fctconnect.resources;
 
 import com.google.cloud.datastore.*;
 import com.google.gson.Gson;
-import org.apache.commons.codec.digest.DigestUtils;
-import pt.unl.fct.di.apdc.chatfct.fctconnect.util.AuthToken;
-import pt.unl.fct.di.apdc.chatfct.fctconnect.util.NewPasswordData;
+import io.jsonwebtoken.JwtException;
+import pt.unl.fct.di.apdc.chatfct.fctconnect.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
@@ -23,89 +22,106 @@ public class NewPasswordResource {
 
     private static final Logger LOG = Logger.getLogger(NewPasswordResource.class.getName());
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    private final Gson g = new Gson();
+    private final KeyFactory userKeyFactory = datastore.newKeyFactory().setKind(DatastoreTypes.USER_TYPE);
+    private final Gson gson = new Gson();
 
     public NewPasswordResource() {
-    }
-
-    private String hashPass(String pass) {
-        return DigestUtils.sha3_512Hex(pass);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response doNewPassword(NewPasswordData data, @Context HttpHeaders headers, @Context HttpServletRequest request) {
         LOG.fine("User attempt to change password");
-        if (data == null || !data.validateData()) {
-            LOG.fine("Invalid data: at least one field is null");
-            return Response.status(Response.Status.BAD_REQUEST).entity(g.toJson("Bad Request - Invalid data")).build();
-        } else if (!data.validateChanger()) {
-            LOG.fine("Cant change others password");
-            return Response.status(Response.Status.FORBIDDEN).entity(g.toJson("Forbidden - Cant change others password")).build();
+        final String token = TokenUtils.extractTokenFromHeaders(request);
+        TokenInfo tokenInfo = verifyToken(token);
+        if (tokenInfo == null) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity(gson.toJson("Invalid credentials")).build();
         }
-        Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
-        String headerToken = AuthToken.getAuthHeader(request);
-        if (headerToken == null) {
-            LOG.fine("Wrong credentials/token - no auth header or invalid auth type");
-            return Response.status(Response.Status.UNAUTHORIZED).entity(g.toJson("Invalid credentials")).build();
+        LOG.fine("Valid token. Proceeding...");
+        final String username = tokenInfo.getUsername();
+        final Response checkData = checkData(data);
+        if (checkData != null) {
+            return checkData;
         }
-        Key loginAuthTokenKey = datastore.newKeyFactory()
-                .addAncestors(PathElement.of("User", data.username)).setKind("AuthToken").newKey(headerToken);
+        Key key = userKeyFactory.newKey(username);
         Transaction txn = datastore.newTransaction();
         try {
-            Entity tokenOnDB = txn.get(loginAuthTokenKey);
-            if (tokenOnDB == null) {
-                LOG.fine("Wrong credentials/token - not found");
+            Entity userOnDB = txn.get(key);
+            final Response checkUserOnDB = checkUserOnDB(userOnDB);
+            if (checkUserOnDB != null) {
                 txn.rollback();
-                return Response.status(Response.Status.UNAUTHORIZED).entity(g.toJson("Invalid credentials")).build();
-            } else {
-                boolean isTokenValid = AuthToken.isValid(tokenOnDB.getLong("expirationDate"), tokenOnDB.getBoolean("isRevoked"));
-                if (!isTokenValid) {
-                    LOG.fine("Expired token");
-                    txn.rollback();
-                    return Response.status(Response.Status.UNAUTHORIZED).entity(g.toJson("Invalid credentials")).build();
-                }
-                LOG.fine("Valid token - proceeding");
+                return checkUserOnDB;
             }
-            Entity userOnDB = txn.get(userKey);
-            if (userOnDB == null) {
-                LOG.fine("User dont exist");
+            final Response checkPassword = checkPassword(userOnDB, data);
+            if (checkPassword != null) {
                 txn.rollback();
-                return Response.status(Response.Status.NOT_FOUND).entity(g.toJson("Not Found - User dont exist")).build();
+                return checkPassword;
             }
-            final String passwordOnDB = userOnDB.getString("password");
-            final String hashedPass = hashPass(data.password);
-            if (!passwordOnDB.equals(hashedPass)) {
-                LOG.fine("Incorrect password");
-                txn.rollback();
-                return Response.status(Response.Status.UNAUTHORIZED).entity(g.toJson("Unauthorized - Incorrect password")).build();
-            } else if (!data.validatePasswords()) {
-                LOG.fine("Passwords dont match");
-                txn.rollback();
-                return Response.status(Response.Status.BAD_REQUEST).entity(g.toJson("Bad Request - Passwords dont match")).build();
-            } else if (!data.validatePasswordConstraints()) {
-                LOG.fine("Passwords dont meet constraints");
-                txn.rollback();
-                return Response.status(Response.Status.BAD_REQUEST).entity(g.toJson("Bad Request - password dont meet constraints")).build();
-            }
-            Entity.Builder userChangedBuilder = Entity.newBuilder(txn.get(userKey));
-            final String hashedNewPass = hashPass(data.newPassword);
-            Entity userChanged = userChangedBuilder
-                    .set("password", hashedNewPass)
-                    .build();
-            txn.put(userChanged);
-            LOG.fine("Password changed successfully");
+            Entity userChanged = updatePassword(userOnDB, data.newPassword);
+            txn.update(userChanged);
             txn.commit();
-            return Response.ok(g.toJson("Password changed successfully")).build();
+            LOG.fine("Password changed successfully");
+            return Response.ok(gson.toJson("Password changed successfully")).build();
         } catch (Exception e) {
             txn.rollback();
             LOG.severe(e.getLocalizedMessage());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(g.toJson("Server Error")).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(gson.toJson("Server Error")).build();
         } finally {
             if (txn.isActive()) {
                 txn.rollback();
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(g.toJson("Server Error")).build();
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(gson.toJson("Server Error")).build();
             }
         }
+    }
+
+    private TokenInfo verifyToken(final String token) {
+        try {
+            return TokenUtils.verifyToken(token);
+        } catch (JwtException ex) {
+            LOG.warning("Invalid token --> " + ex.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    private Response checkData(NewPasswordData data) {
+        if (data == null || !data.validateData()) {
+            LOG.fine("Invalid data: at least one required field is null");
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson("Bad Request - invalid data")).build();
+        }
+        return null;
+    }
+
+    private Response checkUserOnDB(Entity userOnDB) {
+        if (userOnDB == null) {
+            LOG.fine("User does not exist");
+            return Response.status(Response.Status.NOT_FOUND).entity(gson.toJson("Not Found - username is not recognized")).build();
+        }
+        return null;
+    }
+
+    private Response checkPassword(Entity userOnDB, NewPasswordData data) {
+        final String passwordOnDB = userOnDB.getString(DatastoreTypes.PASSWORD_ATTR);
+        final String hashedPassword = PasswordUtils.hashPass(data.password);
+        if (!passwordOnDB.equals(hashedPassword)) {
+            LOG.fine("Incorrect password");
+            return Response.status(Response.Status.UNAUTHORIZED).entity(gson.toJson("Unauthorized - Incorrect password")).build();
+        } else if (data.isPasswordSameAsOld()) {
+            LOG.fine("New password is equal to old password");
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson("Bad Request - new password cant be the same as old password")).build();
+        } else if (!data.comparePasswords()) {
+            LOG.fine("Passwords dont match");
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson("Bad Request - passwords dont match")).build();
+        } else if (!data.validatePassword()) {
+            LOG.fine("Password dont meet constraints");
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson("Bad Request - password dont meet constraints")).build();
+        }
+        return null;
+    }
+
+    private Entity updatePassword(Entity user, String newPass) {
+        final String hashedPassword = PasswordUtils.hashPass(newPass);
+        return Entity.newBuilder(user)
+                .set(DatastoreTypes.PASSWORD_ATTR, hashedPassword)
+                .build();
     }
 }
