8247e94c0ca19bb120c996b5442cc4bd4f61ac97
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-06-17 14:00:58 +0100

    Fixed typo

---
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/PhotoData.java
@@ -0,0 +1,38 @@
+package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+public class PhotoData {
+
+    private static final String EXTENSION_SEPARATOR = ".";
+    private final InputStream bytes;
+    private final String fullFileName;
+
+    public PhotoData(InputStream bytes, String fullFileName) {
+        this.bytes = bytes;
+        this.fullFileName = fullFileName;
+    }
+
+    public byte[] getBytes() throws IOException {
+        return bytes.readAllBytes();
+    }
+
+    public String getFileExtension() {
+        final String filename = Paths.get(fullFileName).getFileName().toString();
+        final int dotIndex = filename.lastIndexOf(EXTENSION_SEPARATOR);
+        if (dotIndex != -1) {
+            return filename.substring(dotIndex);
+        }
+        throw new RuntimeException("Invalid filename");
+    }
+
+    @Override
+    public String toString() {
+        return "PhotoData{" +
+                "bytes=" + bytes +
+                ", fullFileName='" + fullFileName + '\'' +
+                '}';
+    }
+}
