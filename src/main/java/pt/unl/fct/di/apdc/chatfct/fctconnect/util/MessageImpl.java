3a5f1d780c82a8211197f90fd6b4bceccf821a94
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-06-18 13:13:39 +0100

    Added dependencies to apache commons email

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/MessageImpl.java
+++
@@ -1,7 +0,0 @@
-package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
-
-public class MessageImpl {
-
-    private String raw;
-
-}
--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/RegisterEmailConfirmationUtils.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/RegisterEmailConfirmationUtils.java
@@ -1,73 +1,32 @@
 package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
 
-import com.google.api.client.googleapis.json.GoogleJsonError;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+public final class RegisterEmailConfirmationUtils {
 
-public class RegisterEmailConfirmationUtils {
+    private static final String SMTP_SERVER = "smtp.gmail.com";
+    private static final int SMTP_PORT = 587;
 
-    public static MimeMessage createEmail(String toEmailAddress, String fromEmailAddress, String subject, String bodyText) throws MessagingException {
-        Session session = Session.getDefaultInstance(new Properties(), null);
-        MimeMessage email = new MimeMessage(session);
-        email.setFrom(new InternetAddress(fromEmailAddress));
-        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(toEmailAddress));
-        email.setSubject(subject);
-        email.setText(bodyText);
-        return email;
+    public void sendEmail(String userEmail) throws EmailException {
+        Email email = new SimpleEmail();
+        email.setHostName(SMTP_SERVER);
+        email.setSmtpPort(SMTP_PORT);
+        email.setStartTLSEnabled(true);
+        email.setAuthenticator(new DefaultAuthenticator("your-email@gmail.com", "your-password"));
+
+
+        // Set the sender and recipient
+        email.setFrom("your-email@gmail.com");
+        email.addTo(userEmail);
+        email.setSubject("Test Email");
+        email.setMsg("This is a test email.");
+
+        // Send the email
+        email.send();
     }
 
-    public static Message sendEmail(MimeMessage email, String fromEmailAddress, String toEmailAddress) throws MessagingException, IOException {
-        /* Load pre-authorized user credentials from the environment.
-           TODO(developer) - See https://developers.google.com/identity for
-            guides on implementing OAuth2 for your application.*/
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-                .createScoped(GmailScopes.GMAIL_SEND);
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
 
-        // Create the gmail API client
-        Gmail service = new Gmail.Builder(new NetHttpTransport(),
-                GsonFactory.getDefaultInstance(),
-                requestInitializer)
-                .setApplicationName("Gmail Service")
-                .build();
-
-        // Encode and wrap the MIME message into a gmail message
-//        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-//        email.writeTo(buffer);
-//        byte[] rawMessageBytes = buffer.toByteArray();
-//        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
-//        Message message = new MessageImpl();
-//        message.setRaw(encodedEmail);
-
-//        try {
-//            // Create send message
-//            message = service.users().messages().send("me", message).execute();
-//            return message;
-//        } catch (GoogleJsonResponseException e) {
-//            // TODO(developer) - handle error appropriately
-//            GoogleJsonError error = e.getDetails();
-//            if (error.getCode() == 403) {
-//                System.err.println("Unable to send message: " + e.getDetails());
-//            } else {
-//                throw e;
-//            }
-//        }
-        return null;
-    }
 }
