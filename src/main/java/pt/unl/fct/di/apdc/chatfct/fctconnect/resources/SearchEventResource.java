a30bf2c92fc607fb4ab113168d5c81f7359e8bef
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-06-20 01:32:39 +0100

    Added search event by name operation - implement fuzzy search with apache lucene

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/SearchEventResource.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/SearchEventResource.java
@@ -4,7 +4,6 @@ import com.google.cloud.datastore.*;
 import com.google.gson.Gson;
 import io.jsonwebtoken.JwtException;
 import pt.unl.fct.di.apdc.chatfct.fctconnect.util.DatastoreTypes;
-import pt.unl.fct.di.apdc.chatfct.fctconnect.util.ListedEvent;
 import pt.unl.fct.di.apdc.chatfct.fctconnect.util.TokenInfo;
 import pt.unl.fct.di.apdc.chatfct.fctconnect.util.TokenUtils;
 
@@ -12,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -20,21 +20,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-@Path("/listevents")
+@Path("/searchevent")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-public class ListEventsResource {
+public class SearchEventResource {
 
-    private static final Logger LOG = Logger.getLogger(ListEventsResource.class.getName());
+    private static final String EVENT_NAME_QUERY_PARAM = "name";
+    private static final Logger LOG = Logger.getLogger(SearchEventResource.class.getName());
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     private final KeyFactory userKeyFactory = datastore.newKeyFactory().setKind(DatastoreTypes.USER_TYPE);
     private final Gson gson = new Gson();
 
-    public ListEventsResource() {
+    public SearchEventResource() {
     }
 
     @GET
-    public Response doListEvents(@Context HttpHeaders headers, @Context HttpServletRequest request) {
-        LOG.fine("User attempt to list events");
+    public Response doSearchEvent(@QueryParam(EVENT_NAME_QUERY_PARAM) String eventNameFuzzy, @Context HttpHeaders headers, @Context HttpServletRequest request) {
+        LOG.fine("User attempt to search event by name");
         final String token = TokenUtils.extractTokenFromHeaders(request);
         TokenInfo tokenInfo = verifyToken(token);
         if (tokenInfo == null) {
@@ -43,6 +44,10 @@ public class ListEventsResource {
         LOG.fine("Valid token. Proceeding...");
         final String username = tokenInfo.getUsername();
         final String role = tokenInfo.getRole();
+        final Response checkData = checkData(eventNameFuzzy);
+        if (checkData != null) {
+            return checkData;
+        }
         Key userKey = userKeyFactory.newKey(username);
         Transaction txn = datastore.newTransaction();
         try {
@@ -54,10 +59,11 @@ public class ListEventsResource {
             }
             Query<Entity> userEventsQuery = getUserEventsQuery(role);
             QueryResults<Entity> userEventsResults = txn.run(userEventsQuery);
-            List<ListedEvent> userEvents = getUserEvents(userEventsResults);
+            List<String> allEventsNames = getAllEventsNames(userEventsResults);
+            // TODO fuzzy search - with apache lucene
             txn.commit();
-            LOG.info("Events were successfully fetched");
-            return Response.ok(gson.toJson(userEvents)).build();
+            LOG.info("Event was fetched by name");
+            return Response.ok(gson.toJson(allEventsNames)).build();
         } catch (Exception e) {
             txn.rollback();
             LOG.severe(e.getLocalizedMessage());
@@ -68,6 +74,14 @@ public class ListEventsResource {
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(gson.toJson("Server Error")).build();
             }
         }
+    }
+
+    private Response checkData(String eventNameFuzzy) {
+        if (eventNameFuzzy == null || eventNameFuzzy.isBlank()) {
+            LOG.fine("Invalid data: invalid query parameter");
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson("Bad Request - invalid data")).build();
+        }
+        return null;
     }
 
     private Response checkUserOnDB(Entity userOnDB) {
@@ -85,10 +99,10 @@ public class ListEventsResource {
                 .build();
     }
 
-    private List<ListedEvent> getUserEvents(QueryResults<Entity> userEventsResults) {
-        List<ListedEvent> res = new ArrayList<>();
-        userEventsResults.forEachRemaining(event -> res.add(ListedEvent.createListedEvent(event)));
-        return res;
+    private List<String> getAllEventsNames(QueryResults<Entity> userEventsResults) {
+        List<String> names = new ArrayList<>();
+        userEventsResults.forEachRemaining(event -> names.add(event.getString(DatastoreTypes.EVENT_NAME_ATTR)));
+        return names;
     }
 
     private TokenInfo verifyToken(final String token) {
