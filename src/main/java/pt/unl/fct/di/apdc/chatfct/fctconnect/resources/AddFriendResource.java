cbb768561e4bfad10a927356e98a9bdf46c5f753
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-04 15:29:57 +0100

    Added cache to add friend operation

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/AddFriendResource.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/AddFriendResource.java
@@ -24,9 +24,19 @@ public class AddFriendResource {
     private static final Logger LOG = Logger.getLogger(AddFriendResource.class.getName());
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     private final KeyFactory userKeyFactory = datastore.newKeyFactory().setKind(DatastoreTypes.USER_TYPE);
+    private final MemcacheUtils memcacheUsers = MemcacheUtils.getMemcache(MemcacheUtils.USER_NAMESPACE);
     private final Gson gson = new Gson();
 
     public AddFriendResource() {
+    }
+
+    private Entity getUserCached(String username) {
+        final String key = String.format(MemcacheUtils.USER_ENTITY_KEY, username);
+        return memcacheUsers.get(key, Entity.class);
+    }
+
+    private boolean isCached(Entity e) {
+        return e != null;
     }
 
     @POST
@@ -48,12 +58,33 @@ public class AddFriendResource {
         Key otherKey = userKeyFactory.newKey(data.friendUsername);
         Transaction txn = datastore.newTransaction();
         try {
-            Entity usernameOnDB = txn.get(usernameKey);
-            Entity otherOnDB = txn.get(otherKey);
-            final Response checkUsersOnDB = checkUsersOnDB(usernameOnDB, otherOnDB);
-            if (checkUsersOnDB != null) {
-                txn.rollback();
-                return checkUsersOnDB;
+            Entity usernameOnDB;
+            final Entity userCached = getUserCached(username);
+            final boolean isUserCached = isCached(userCached);
+            if (isUserCached) {
+                usernameOnDB = userCached;
+            } else {
+                usernameOnDB = txn.get(usernameKey);
+                final Response checkUserOnDB = checkUserOnDB(usernameOnDB);
+                if (checkUserOnDB != null) {
+                    txn.rollback();
+                    return checkUserOnDB;
+                }
+                memcacheUsers.put(String.format(MemcacheUtils.USER_ENTITY_KEY, username), usernameOnDB);
+            }
+            Entity otherOnDB;
+            final Entity otherCached = getUserCached(data.friendUsername);
+            final boolean isOtherCached = isCached(otherCached);
+            if (isOtherCached) {
+                otherOnDB = otherCached;
+            } else {
+                otherOnDB = txn.get(otherKey);
+                final Response checkOtherOnDB = checkUserOnDB(otherOnDB);
+                if (checkOtherOnDB != null) {
+                    txn.rollback();
+                    return checkOtherOnDB;
+                }
+                memcacheUsers.put(String.format(MemcacheUtils.USER_ENTITY_KEY, data.friendUsername), otherOnDB);
             }
             final Response postFriendDBRequest = RestClientUtils.postFriend(data);
             if (postFriendDBRequest.getStatus() == Response.Status.OK.getStatusCode()) {
@@ -87,10 +118,10 @@ public class AddFriendResource {
         return null;
     }
 
-    private Response checkUsersOnDB(Entity usernameOnDB, Entity otherOnDB) {
-        if (usernameOnDB == null || otherOnDB == null) {
-            LOG.fine("At least one of the users dont exist");
-            return Response.status(Response.Status.NOT_FOUND).entity(gson.toJson("Not Found - At least one of the users dont exist")).build();
+    private Response checkUserOnDB(Entity user) {
+        if (user == null) {
+            LOG.fine("User does not exist");
+            return Response.status(Response.Status.NOT_FOUND).entity(gson.toJson("Not Found - username is not recognized")).build();
         }
         return null;
     }
