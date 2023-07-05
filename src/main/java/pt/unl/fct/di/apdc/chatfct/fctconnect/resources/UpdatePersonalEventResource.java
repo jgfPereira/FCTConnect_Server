79b21be90b9884be05ccab7fe5fbe87eeecf020a
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-05 23:24:16 +0100

    Added cache to update personal event op

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/UpdatePersonalEventResource.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/resources/UpdatePersonalEventResource.java
@@ -27,9 +27,25 @@ public class UpdatePersonalEventResource {
     private static final String SEPARATOR = " ";
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     private final KeyFactory userKeyFactory = datastore.newKeyFactory().setKind(DatastoreTypes.USER_TYPE);
+    private final MemcacheUtils memcacheUsers = MemcacheUtils.getMemcache(MemcacheUtils.USER_NAMESPACE);
+    private final MemcacheUtils memcachePersonalEvents = MemcacheUtils.getMemcache(MemcacheUtils.PERSONAL_EVENTS_NAMESPACE);
     private final Gson gson = new Gson();
 
     public UpdatePersonalEventResource() {
+    }
+
+    private Entity getUserCached(String username) {
+        final String key = String.format(MemcacheUtils.USER_ENTITY_KEY, username);
+        return memcacheUsers.get(key, Entity.class);
+    }
+
+    private Entity getPersonalEventCached(String id) {
+        final String key = String.format(MemcacheUtils.PERSONAL_EVENT_ENTITY_KEY, id);
+        return memcachePersonalEvents.get(key, Entity.class);
+    }
+
+    private boolean isCached(Entity e) {
+        return e != null;
     }
 
     @PUT
@@ -51,22 +67,39 @@ public class UpdatePersonalEventResource {
         Key personalEventKey = createPersonalEventKey(username, data.id);
         Transaction txn = datastore.newTransaction();
         try {
-            final Entity userOnDB = txn.get(userKey);
-            final Response checkUserOnDB = checkUserOnDB(userOnDB);
-            if (checkUserOnDB != null) {
-                txn.rollback();
-                return checkUserOnDB;
+            Entity userOnDB;
+            final Entity userCached = getUserCached(username);
+            final boolean isUserCached = isCached(userCached);
+            if (isUserCached) {
+                userOnDB = userCached;
+            } else {
+                userOnDB = txn.get(userKey);
+                final Response checkUserOnDB = checkUserOnDB(userOnDB);
+                if (checkUserOnDB != null) {
+                    txn.rollback();
+                    return checkUserOnDB;
+                }
+                memcacheUsers.put(String.format(MemcacheUtils.USER_ENTITY_KEY, username), userOnDB);
             }
-            final Entity personalEventOnDB = txn.get(personalEventKey);
-            final Response checkPersonalEventOnDB = checkPersonalEventOnDB(personalEventOnDB);
-            if (checkPersonalEventOnDB != null) {
-                txn.rollback();
-                return checkPersonalEventOnDB;
+            Entity personalEventOnDB;
+            final Entity personalEventCached = getPersonalEventCached(data.id);
+            final boolean isPersonalEventCached = isCached(personalEventCached);
+            if (isPersonalEventCached) {
+                personalEventOnDB = personalEventCached;
+            } else {
+                personalEventOnDB = txn.get(personalEventKey);
+                final Response checkPersonalEventOnDB = checkPersonalEventOnDB(personalEventOnDB);
+                if (checkPersonalEventOnDB != null) {
+                    txn.rollback();
+                    return checkPersonalEventOnDB;
+                }
+                memcachePersonalEvents.put(String.format(MemcacheUtils.PERSONAL_EVENT_ENTITY_KEY, data.id), personalEventOnDB);
             }
             final List<String> invalidFormatUpdates = new ArrayList<>();
             final Entity updatePersonalEvent = updatePersonalEvent(personalEventOnDB, data, invalidFormatUpdates);
             if (didPersonalEventChanged(invalidFormatUpdates, data.updateEntries)) {
                 txn.update(updatePersonalEvent);
+                memcachePersonalEvents.put(String.format(MemcacheUtils.PERSONAL_EVENT_ENTITY_KEY, data.id), updatePersonalEvent);
             }
             txn.commit();
             LOG.info("Personal event was updated - if format checked out");
