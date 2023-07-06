640509fabe1e194e46338b53add472a293855ffd
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-06 13:23:43 +0100

    Added dependencies of jsoup for scraping and created utils class

---
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/JsoupUtils.java
@@ -0,0 +1,21 @@
+package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public final class JsoupUtils {
+
+    private static final String NEWS_BASE_URL = "https://www.fct.unl.pt/noticias";
+    private static final String NEWS_CONTAINER_CLASS = "view view-noticias view-id-noticias view-display-id-page_1 view-dom-id-1";
+    private final Document doc;
+
+    private JsoupUtils() {
+        try {
+            doc = Jsoup.connect(NEWS_BASE_URL).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
