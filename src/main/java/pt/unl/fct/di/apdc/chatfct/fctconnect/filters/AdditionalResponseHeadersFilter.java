745c2457b533c20a8db4c3221ae84da4cf786c03
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-09 20:28:14 +0100

    Change filters on response - allow access control origin - using put instead of add to only have one value

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/filters/AdditionalResponseHeadersFilter.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/filters/AdditionalResponseHeadersFilter.java
@@ -4,6 +4,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import java.util.List;
 
 @Provider
 public class AdditionalResponseHeadersFilter implements ContainerResponseFilter {
@@ -13,7 +14,7 @@ public class AdditionalResponseHeadersFilter implements ContainerResponseFilter 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "HEAD,GET,PUT,POST,DELETE,OPTIONS");
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().put("Access-Control-Allow-Origin", List.of("*"));
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-Auth-Token");
         responseContext.getHeaders().add("Access-Control-Expose-Headers", "*");
     }
