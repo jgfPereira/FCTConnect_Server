fa93dabad0e25da10249fa5b3d7aaf39799b8c30
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-10 16:53:53 +0100

    Fixed conversion of any format to jpeg - using raster and pixel grabber to handle transparency

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/ImageUtils.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/ImageUtils.java
@@ -2,7 +2,8 @@ package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
 
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.image.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,18 +11,26 @@ import java.io.InputStream;
 public final class ImageUtils {
 
     private static final String JPEG_IMAGE_FORMAT = "jpeg";
+    private static final int[] RGB_MASKS = {0xFF0000, 0xFF00, 0xFF};
+    private static final ColorModel RGB_OPAQUE = new DirectColorModel(32, RGB_MASKS[0], RGB_MASKS[1], RGB_MASKS[2]);
 
     private ImageUtils() {
     }
 
     public static byte[] convertToJPEG(InputStream inputStream) {
         try {
-            final BufferedImage image = ImageIO.read(inputStream);
-            final BufferedImage imageNoAlphaChannel = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+            final Image img = Toolkit.getDefaultToolkit().createImage(inputStream.readAllBytes());
+            final PixelGrabber pixelGrabber = new PixelGrabber(img, 0, 0, -1, -1, true);
+            pixelGrabber.grabPixels();
+            final int width = pixelGrabber.getWidth();
+            final int height = pixelGrabber.getHeight();
+            final DataBuffer buffer = new DataBufferInt((int[]) pixelGrabber.getPixels(), width * height);
+            final WritableRaster raster = Raster.createPackedRaster(buffer, width, height, width, RGB_MASKS, null);
+            final BufferedImage bufferedImage = new BufferedImage(RGB_OPAQUE, raster, false, null);
             final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-            ImageIO.write(imageNoAlphaChannel, JPEG_IMAGE_FORMAT, byteStream);
+            ImageIO.write(bufferedImage, JPEG_IMAGE_FORMAT, byteStream);
             return byteStream.toByteArray();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
