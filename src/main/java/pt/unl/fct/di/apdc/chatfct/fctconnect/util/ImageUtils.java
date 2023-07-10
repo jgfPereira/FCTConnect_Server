6f93637ff32537abdfb964afcc2acfd39eeda992
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2023-07-10 03:29:15 +0100

    Removed apache imaging dependency - will use javax.imageio

--- src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/ImageUtils.java
+++ src/main/java/pt/unl/fct/di/apdc/chatfct/fctconnect/util/ImageUtils.java
@@ -1,7 +1,14 @@
 package pt.unl.fct.di.apdc.chatfct.fctconnect.util;
 
+
+import java.io.InputStream;
+
 public final class ImageUtils {
 
     private ImageUtils() {
+    }
+
+    public static void convertToJPEG(InputStream inputStream, String fname) {
+
     }
 }
